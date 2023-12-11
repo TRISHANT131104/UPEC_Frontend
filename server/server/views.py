4e284cd6714de757bc3cddeacf9e16c4047b64ad
PradeepKumarRebbavarapu <wardengaming1807@gmail.com>
2023-12-11 12:56:37 +0530

    reverted changes

--- server/server/views.py
+++
@@ -1,8 +0,0 @@
-from .adapters import CustomGoogleOAuth2Adapter
-from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-from dj_rest_auth.registration.views import SocialLoginView
-
-class GoogleLogin(SocialLoginView): # if you want to use Authorization Code Grant, use this
-    adapter_class = CustomGoogleOAuth2Adapter
-    callback_url = "http://localhost:3000"
-    client_class = OAuth2Client
